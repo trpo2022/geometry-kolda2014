68278f50a77c937ddfd2bd94a2c5736b5762e373
Колдаев Андрей Юрьевич <ip111s13@awp05.csc.local>
2022-04-27 12:50:21 +0700

    Auto Build Check

--- src/libgeometry/Check.h
+++ src/libgeometry/Check.h
@@ -1,10 +1,55 @@
 #ifndef function
 #define function
 
-void check(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-void circle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-void circle_perimeter(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-void triangle_area(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
-void triangle_perimeter(float x_cir, float y_cir, float r, float x1_tr, float y1_tr, float x2_tr, float y2_tr, float x3_tr, float y3_tr);
+void check(
+        float x_cir,
+        float y_cir,
+        float r,
+        float x1_tr,
+        float y1_tr,
+        float x2_tr,
+        float y2_tr,
+        float x3_tr,
+        float y3_tr);
+void circle_area(
+        float x_cir,
+        float y_cir,
+        float r,
+        float x1_tr,
+        float y1_tr,
+        float x2_tr,
+        float y2_tr,
+        float x3_tr,
+        float y3_tr);
+void circle_perimeter(
+        float x_cir,
+        float y_cir,
+        float r,
+        float x1_tr,
+        float y1_tr,
+        float x2_tr,
+        float y2_tr,
+        float x3_tr,
+        float y3_tr);
+void triangle_area(
+        float x_cir,
+        float y_cir,
+        float r,
+        float x1_tr,
+        float y1_tr,
+        float x2_tr,
+        float y2_tr,
+        float x3_tr,
+        float y3_tr);
+void triangle_perimeter(
+        float x_cir,
+        float y_cir,
+        float r,
+        float x1_tr,
+        float y1_tr,
+        float x2_tr,
+        float y2_tr,
+        float x3_tr,
+        float y3_tr);
 
 #endif
