44f8bf0d63252f77530e0ac3e43d24dd17ddca75
Andrey Koldaev <koldand@users.noreply.github.com>
2022-04-30 12:50:00 +0700

    Found and fixed an input error

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,26 +1,34 @@
 #include "libgeometry/Check.h"
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main()
+int main(void)
 {
-    float x_cir, y_cir, x1_tr, x2_tr, x3_tr, y1_tr, y2_tr, y3_tr, r;
-    printf("Enter the coordinates of the circle: ");
-    scanf("%f %f %f", &x_cir, &y_cir, &r);
-    printf("\nEnter the coordinates x1, y1 for the triangle: ");
-    scanf("%f %f", &x1_tr, &y1_tr);
-    printf("\nEnter the coordinates x2, y2 for the triangle: ");
-    scanf("%f %f", &x2_tr, &y2_tr);
-    printf("\nEnter the coordinates x3, y3 for the triangle: ");
-    scanf("%f %f", &x3_tr, &y3_tr);
-    if (r > 0) {
-        check(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
-    } else {
-        printf("Error! Radius is negative");
+    int n_str = 50;
+    int n_re = 10;
+    char str[n_str];
+    char re[n_re];
+
+    while (1) {
+        fgets(str, 50, stdin);
+        str[strlen(str) - 1] = '\0';
+        sscanf(str, "%[^(]%*[\n]", re);
+
+        for (int i = 0; re[i]; i++)
+            re[i] = tolower(re[i]);
+
+        if (strcmp(re, "circle") == 0) {
+            check_circle(str, re);
+
+        } else if (strcmp(re, "triangle") == 0) {
+            check_triangle(str, re);
+
+        } else
+            printf("Invalid input!\n");
     }
-    circle_area(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
-    circle_perimeter(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
-    triangle_area(x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
-    triangle_perimeter(
-            x_cir, y_cir, r, x1_tr, y1_tr, x2_tr, y2_tr, x3_tr, y3_tr);
+
     return 0;
 }
