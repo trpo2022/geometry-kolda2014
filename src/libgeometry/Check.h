1ad7702b5de4da375df66a0116089c5a2877c7e5
Andrey Koldaev <koldand@users.noreply.github.com>
2022-05-06 09:04:59 +0700

    Correction Check.h

--- src/libgeometry/Check.h
+++ src/libgeometry/Check.h
@@ -3,7 +3,8 @@
 
 int check_circle(char str[], char re[]);
 int check_triangle(char str[], char re[]);
-int triangle_perimeter(float x1, float y1, float x2, float y2, float x3, float y3);
+int triangle_perimeter(
+        float x1, float y1, float x2, float y2, float x3, float y3);
 int triangle_area(float x1, float y1, float x2, float y2, float x3, float y3);
 int circle_perimeter(float r);
 int circle_area(float r);
