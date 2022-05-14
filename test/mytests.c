dce4eb65b5b78f9e57712120764a1316c3064f11
Andrey Koldaev <koldand@users.noreply.github.com>
2022-05-14 13:03:05 +0700

    Tests for circle

--- test/mytests.c
+++ test/mytests.c
@@ -3,7 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-CTEST(ctest, write1)
+CTEST(ctest, 1)
 {
-    ASSERT_EQUAL(0, check_circle((char*)"circle(1 2, 3)", (char*)"circle"));
+    char a[] = "circle(1 2, 3)";
+    char b[] = "circle";
+    ASSERT_EQUAL(0, check_circle(a, b));
 }
+
+CTEST(ctect, 2)
+{
+    char a[] = "circle(1 2,,3)";
+    char b[] = "circle";
+    ASSERT_EQUAL(1, check_circle(a, b));
+}
+
+CTEST(ctect, 3)
+{
+    char a[] = "circle(1,2 3)";
+    char b[] = "circle";
+    ASSERT_EQUAL(1, check_circle(a, b));
+}
+
+CTEST(ctect, 4)
+{
+    char a[] = "circle(1 2 3)";
+    char b[] = "circle";
+    ASSERT_EQUAL(1, check_circle(a, b));
+}
+
+CTEST(ctest, 5)
+{
+    char a[] = "cicle(1 2, 3)";
+    char b[] = "circle";
+    ASSERT_EQUAL(0, check_circle(a, b));
+}
+
+CTEST(ctest, 6)
+{
+    char a[] = "circle(1 2, 3))";
+    char b[] = "circle";
+    ASSERT_EQUAL(1, check_circle(a, b));
+}
+
